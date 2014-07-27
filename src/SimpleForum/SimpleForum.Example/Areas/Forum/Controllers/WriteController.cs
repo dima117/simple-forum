d540507a22e8488b04448e79af68ccdc8da49507
dima117 <dima117a@gmail.com>
2014-07-27 16:38:18 +0400

    - страницы-заглушки для создания новых топиков/комментариев и страница-заглушка для аутентификации;

---
+++ src/SimpleForum/SimpleForum.Example/Areas/Forum/Controllers/WriteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SimpleForum.Web.Controllers
+{
+	[Authorize]
+    public class WriteController : Controller
+    {
+		[HttpGet]
+        public ActionResult Topic(Guid id)
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public ActionResult Reply(Guid id)
+        {
+            return View();
+        }
+    }
+}
