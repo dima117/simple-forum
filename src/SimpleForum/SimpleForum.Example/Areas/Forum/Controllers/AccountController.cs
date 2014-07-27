711b512548f735b05cbe5ef8d9990d01cae68bcb
dima117 <dima117a@gmail.com>
2014-07-27 17:50:07 +0400

    - аутентификация;

--- src/SimpleForum/SimpleForum.Example/Areas/Forum/Controllers/AccountController.cs
+++ src/SimpleForum/SimpleForum.Example/Areas/Forum/Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using SimpleForum.Domain;
+using SimpleForum.Web.Models.Account;
 
 namespace SimpleForum.Web.Controllers
 {
@@ -11,10 +14,33 @@ namespace SimpleForum.Web.Controllers
         //
         // GET: /Account/
 
+		[HttpGet]
         public ActionResult Login()
         {
-            return View();
+	        var model = new LoginModel();
+			return View(model);
         }
+
+		[HttpPost]
+		public ActionResult Login(LoginModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				using (var db = new SimpleForumDbContext())
+				{
+					var user = db.Users.FirstOrDefault(u => u.Email == model.Email);
+
+					if (user != null)
+					{
+						FormsAuthentication.RedirectFromLoginPage(user.Email, model.RememberMe);
+					}
+
+					model.HesError = true;
+				}
+			}
+
+			return View(model);
+		}
 
     }
 }
---
+++ src/SimpleForum/SimpleForum.Example/Areas/Forum/Models/Account/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleForum.Web.Models.Account
+{
+	public class LoginModel
+	{
+		[Required, DisplayName("Email address")]
+		public string Email { get; set; }
+
+		[Required, DisplayName("Password")]
+		public string Password { get; set; }
+
+		public bool RememberMe { get; set; }
+		public bool HesError { get; set; }
+	}
+}
