1f7422bdd8896c7a695d153f8fada05abbeee8bd
dima117 <dima117a@gmail.com>
2014-07-30 06:31:42 +0400

    - добавление тем на форум;

--- src/SimpleForum/SimpleForum.Example/Areas/Forum/Controllers/WriteController.cs
+++ src/SimpleForum/SimpleForum.Example/Areas/Forum/Controllers/WriteController.cs
@@ -8,15 +8,15 @@ using SimpleForum.Web.Models.Write;
 namespace SimpleForum.Web.Controllers
 {
 	[Authorize]
-    public class WriteController : Controller
-    {
+	public class WriteController : Controller
+	{
 		[HttpGet]
-        public ActionResult Topic(Guid id)
-        {
+		public ActionResult Topic(Guid id)
+		{
 			using (var db = new SimpleForumDbContext())
 			{
 				var section = db.Set<Section>().Single(s => s.Id == id);
-				
+
 				var model = new TopicModel
 				{
 					SectionTitle = section.Title
@@ -24,14 +24,49 @@ namespace SimpleForum.Web.Controllers
 
 				return View(model);
 			}
+		}
 
-           
-        }
+		[HttpPost]
+		public ActionResult Topic(Guid id, TopicModel model)
+		{
+			using (var db = new SimpleForumDbContext())
+			{
+				var user = db.Users.Single(u => u.Email == User.Identity.Name);
+				var section = db.Set<Section>().Single(s => s.Id == id);
+
+				if (!ModelState.IsValid)
+				{
+					model.SectionTitle = section.Title;
+					return View(model);
+				}
+
+				Guid topicId = Guid.NewGuid();
+
+				var topic = new Topic
+				{
+					Id = topicId,
+					Subject = model.Subject,
+					Text = model.Text,
+					Created = DateTime.Now,
+
+					AuthorId = user.Id,
+					SectionId = section.Id
+				};
+
+				db.Set<Topic>().Add(topic);
+				db.SaveChanges();
+
+				return RedirectToAction("Index", "Topic", new { id = topicId });
+			}
+		}
 
 		[HttpGet]
 		public ActionResult Reply(Guid id)
-        {
-            return View();
-        }
-    }
+		{
+
+
+
+			return View();
+		}
+	}
 }
