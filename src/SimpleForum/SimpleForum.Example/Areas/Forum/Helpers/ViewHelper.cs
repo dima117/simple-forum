78116a5b922ca06279890789caac6edba0ce3221
dima117 <dima117a@gmail.com>
2014-08-14 22:52:53 +0400

    - отключен markdown, добавлена более простая обработка текста;

---
+++ src/SimpleForum/SimpleForum.Example/Areas/Forum/Helpers/ViewHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleForum.Web.Helpers
+{
+	public static class ViewHelper
+	{
+		private const string PATTERN = @"(https?|ftp|file)\://[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\*\-\._~%]*)*";
+		private const string REPLACEMENT = @"<a href=""$0"" rel=""nofollow"" target=""_blank"">$0</a>";
+		private readonly static Regex regex = new Regex(PATTERN, RegexOptions.IgnoreCase);
+
+		public static IHtmlString PrepareText(this HtmlHelper helper, string text)
+		{
+			var str = HttpUtility.HtmlEncode(text ?? string.Empty);
+			var html = regex.Replace(str, REPLACEMENT).Replace("\r\n", "<br />");
+
+			return new MvcHtmlString(html);
+		}
+	}
+}
